55d23a8545e405044fa487cbc7280e0df9399306
Guilherme Baccarin <62629900+Baccarin@users.noreply.github.com>
2020-09-21 21:24:11 -0300

    Plot

--- Plot/plot-barras-insertion.py
+++ Plot/plot-barras-insertion.py
@@ -9,7 +9,7 @@ insertion10000 = round((108+100+94+115+109)/5)/100
 insertion50000 = round((1546+1564+1256+1548+1499)/5)/100
 insertion100000 = round((6978+7631+7299+7456+7591)/5)/100
 insertion150000 = round((7889+7954+8215+8125+7959)/5)/100
-insertion250000 = round((2451+2652+2956+3214+3102)/5)/100
+insertion250000 = round((12451+13652+10956+11214+11102)/5)/100
 insertion500000 = round((31254+32653+33652+36524+37854)/5)/100
 
 insertionY=np.array([insertion1,insertion10,insertion100,insertion1000,insertion10000,insertion50000,insertion100000,insertion150000,insertion250000,insertion500000])
@@ -19,7 +19,7 @@ x1 =  np.arange(len(insertionY))
 plt.ylabel('Tempo em segundos')
 plt.xlabel('Número de entradas')
 
-plt.bar(x1,insertionY,width=0.25, label = 'Produto A', color = 'b')
+plt.bar(x1,insertionY,width=0.25, label = 'Insertion', color = 'b')
 
 
 plt.legend()
--- Plot/plot-linhas.py
+++ Plot/plot-linhas.py
@@ -1,7 +1,9 @@
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
 
-X=np.array([1,2,3,4,5,6,7,8,9,10])
+fig = plt.figure(figsize=(10, 8))
 
 insertion1 = round((0+0+0+0+0)/5)/100
 insertion10 = round((0+0+0+0+0)/5)/100
@@ -11,7 +13,7 @@ insertion10000 = round((108+100+94+115+109)/5)/100
 insertion50000 = round((1546+1564+1256+1548+1499)/5)/100
 insertion100000 = round((6978+7631+7299+7456+7591)/5)/100
 insertion150000 = round((7889+7954+8215+8125+7959)/5)/100
-insertion250000 = round((2451+2652+2956+3214+3102)/5)/100
+insertion250000 = round((12451+13652+10956+11214+11102)/5)/100
 insertion500000 = round((31254+32653+33652+36524+37854)/5)/100
 
 insertionY=np.array([insertion1,insertion10,insertion100,insertion1000,insertion10000,insertion50000,insertion100000,insertion150000,insertion250000,insertion500000])
@@ -24,8 +26,8 @@ selection10000 = round((134+125+145+128+130)/5)/100
 selection50000 = round((3727+3763+3345+3456+3655)/5)/100
 selection100000 = round((12600+11526+13465+12546+11546)/5)/100
 selection150000 = round((32500+31658+33264+31254+32653)/5)/100
-selection250000 = round((8379+82659+84565+84545+83646)/5)/100
-selection500000 = round(360000/100)
+selection250000 = round((38379+38259+38465+38445+38646)/5)/100
+selection500000 = round(46000/100)
 
 selectionY=np.array([selection1,selection10,selection100,selection1000,selection10000,selection50000,selection100000,selection150000,selection250000,selection500000])
 
@@ -42,15 +44,19 @@ merge500000 = round((190+150+151+160+177)/5)/100
 
 mergeY=np.array([merge1,merge10,merge100,merge1000,merge10000,merge50000,merge100000,merge150000,merge250000,merge500000])
 
+X=np.array([1,2,3,4,5,6,7,8,9,10])
+
 plt.ylabel('Tempo em segundos')
 plt.xlabel('Número de entradas')
 
-plt.plot(X,insertionY,'g--', label='insertion')
+plt.plot(X,insertionY,'b--', label='insertion')
 
-plt.plot(X,selectionY,'b--', label='selection')
+plt.plot(X,selectionY,'y--', label='selection')
 
-plt.plot(X,mergeY,'r--',label='merge')
+plt.plot(X,mergeY,'g--',label='merge')
 
 plt.legend()
 plt.show()
+
+fig.savefig('linhastotaisplot.png')
 
